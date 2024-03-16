4c2c565a21ae9c471e813be8316b2d0297c23723
Steffi Antony <144442545+Steffi7182@users.noreply.github.com>
2024-03-16 14:44:06 +0530

    Add files via upload

---
+++ 20.sh
@@ -0,0 +1,12 @@
+#author: Steffi Antony
+
+echo "Enter a number:"
+read num
+fact(){
+    fact=1
+    for((i=1; i<=num; i++)); do 
+        fact=$((fact * i))
+    done
+    echo $fact
+}
+echo "Factorial of $num is: $(fact)"
